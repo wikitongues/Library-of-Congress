1df6da83292f0aad22170f252e549c54ab4e489e
Scott Rohrer <smrohrer@alumni.cmu.edu>
2020-09-13 14:28:52 -0400

    Retrieve all fields from the comprehensive archival view

--- getAirtableMetadata.js
+++ getAirtableMetadata.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// https://github.com/wikitongues/Oral-History-Template-Instantiator/blob/master/single.js
-
 'use strict';
 var Airtable = require('airtable');
 var fs = require('fs');
@@ -13,49 +11,18 @@ try {
   var base = new Airtable({apiKey: process.env.APIKEY}).base(process.env.BASE);
 
   base('🍩 Oral Histories').select({
-      view: ".LOCMetadataView",
+      view: "Archival View (Comprehensive)",
       cellFormat: "string",
       timeZone: "America/New_York",
       userLocale: "en-ca",
       filterByFormula: "Identifier='"+single+"'",
-      fields: [
-        "Identifier",
-        "Languages: ISO Code (639-3)",
-        "Language names",
-        "Languages: Speaker preferred names",
-        "Contributor: Speakers",
-        ".self?",
-        "Creator",
-        "Subject: Language Nation of Origin",
-        "Coverage: Video Territory",
-        "Description",
-        "Rights",
-        "Youtube Publish Date",
-        "Wikimedia Eligibility",
-        "Wiki Commons URL"
-      ]
   }).eachPage(function page(records, fetchNextPage) {
     if (!Array.isArray(records) || !!records.length) {
       records.forEach(function(record) {
           const content = [
             `Metadata for ${record.get('Identifier')}`,
 
-            `Oral History ID:  ${record.get('Identifier')}`,
-            `Languages by ISO 639-3 Code: ${record.get('Languages: ISO Code (639-3)')}`,
-            `Language Names: ${record.get('Language names')}`,
-            `Alternate Names: ${record.get('Languages: Speaker preferred names')}`,
-            `Speakers: ${record.get('Contributor: Speakers')}`,
-
-            `Video Description: ${record.get('Description') || ''}`,
-
-            `Original Submitter: ${record.get('Creator')}`,
-            `Licenses: ${record.get('Rights')}`,
-            `Video Nation: ${record.get('Subject: Language Nation of Origin')}`,
-            `Video Territory: ${record.get('Coverage: Video Territory')}`,
-
-            `Published to Youtube on: ${record.get('Youtube Publish Date')}`,
-            `Wikimedia Status: ${record.get('Wikimedia Eligibility')}`,
-            `Wiki Commons URL: ${record.get('Wiki Commons URL')}`
+            ...Object.keys(record.fields).map(field => `${field}: ${record.get(field)}`)
           ].join('\n');
 
           fs.writeFileSync(`${destination}/loctemp__${single}/${record.get('Identifier')}__metadata.txt`, content);
