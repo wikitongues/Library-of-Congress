3316a99fd71f2a6a126e59800d910df33a4bcc98
Scott Rohrer <smrohrer@alumni.cmu.edu>
2023-11-11 16:57:18 -0500

    Report validation errors on Airtable

--- tasks/download.py
+++ tasks/download.py
@@ -1,4 +1,6 @@
+import re
 from functools import cached_property
+from typing import List
 from zipfile import ZipFile
 
 import dropbox
@@ -6,6 +8,7 @@ import luigi
 
 from .archival_task import ArchivalTask
 from .constants import VALID_VIDEO_EXTENSIONS
+from .enums import ArchivalStatus
 from .exceptions import NoDropboxFolder, NoThumbnail, NoVideo
 
 
@@ -27,7 +30,65 @@ class Download(ArchivalTask):
     def output(self):
         return luigi.LocalTarget(f"{self.local_oh_dir}/{self.oh_id}/")
 
-    def validate(self) -> bool:
+    def has_valid_video(self, filenames: List[str]) -> bool:
+        # Is there any video file in the root, perhaps with a legacy filename?
+        root_videos = list(
+            filter(
+                lambda filename: re.match(
+                    rf"^{re.escape(str(self.dropbox_path))}\/[^\/]+\.(?:{'|'.join(f'(?:{ext})' for ext in VALID_VIDEO_EXTENSIONS)})$",
+                    filename,
+                    re.IGNORECASE,
+                ),
+                filenames,
+            )
+        )
+        if len(root_videos) == 1:
+            return True
+        if len(root_videos) > 1:
+            # Root folder needs to be cleaned up
+            return False
+
+        # A raw video may also be valid assuming the OH is marked as eligible
+        raw_videos = list(
+            filter(
+                lambda filename: re.match(
+                    rf"^{re.escape(str(self.dropbox_path))}\/raws\/footage\/clips\/[^\/]+\.(?:{'|'.join(f'(?:{ext})' for ext in VALID_VIDEO_EXTENSIONS)})$",
+                    filename,
+                    re.IGNORECASE,
+                ),
+                filenames,
+            )
+        )
+        return len(raw_videos) == 1
+
+    def has_valid_thumbnail(self, filenames: List[str]) -> bool:
+        # Is there any jpg file in the root, perhaps with a legacy filename?
+        root_jpgs = list(
+            filter(
+                lambda filename: re.match(
+                    rf"^{re.escape(str(self.dropbox_path))}\/[^\/]+\.jpg$", filename, re.IGNORECASE
+                ),
+                filenames,
+            )
+        )
+        if len(root_jpgs) == 1:
+            return True
+        if len(root_jpgs) > 1:
+            # Root folder needs to be cleaned up
+            return False
+
+        # A raw jpg file may also be valid assuming the OH is marked as eligible
+        raw_jpgs = list(
+            filter(
+                lambda filename: re.match(
+                    rf"^{re.escape(str(self.dropbox_path))}\/raws\/thumbnail\/[^\/]+\.jpg$", filename, re.IGNORECASE
+                ),
+                filenames,
+            )
+        )
+        return len(raw_jpgs) == 1
+
+    def validate(self) -> None:
         try:
             files = []
             lister = self.dbx.files_list_folder(self.dropbox_path, recursive=True)
@@ -40,15 +101,23 @@ class Download(ArchivalTask):
             # TODO differentiate between not found and other error
             raise NoDropboxFolder
 
-        if not any((f.endswith(".jpg") for f in filenames)):
-            self.logger.warn(f"No thumbnail found on Dropbox for {self.oh_id}; skipping.")
-            raise NoThumbnail
-
-        if not any((any((f.endswith(f".{ext}") for ext in VALID_VIDEO_EXTENSIONS)) for f in filenames)):
+        if not self.has_valid_video(filenames):
             self.logger.warn(f"No video found on Dropbox for {self.oh_id}; skipping.")
+
+            self.airtable_client.update_record(
+                self.airtable_record_id, {self.status_field: ArchivalStatus.INVALID_VIDEO.value}
+            )
+
             raise NoVideo
 
-        return True
+        if not self.has_valid_thumbnail(filenames):
+            self.logger.warn(f"No thumbnail found on Dropbox for {self.oh_id}; skipping.")
+
+            self.airtable_client.update_record(
+                self.airtable_record_id, {self.status_field: ArchivalStatus.INVALID_THUMBNAIL.value}
+            )
+
+            raise NoThumbnail
 
     def download(self):
         self.dbx.files_download_zip_to_file(self.zip_path, self.dropbox_path)
